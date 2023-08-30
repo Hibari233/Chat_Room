6f723320617b1d107e481cb34278f32aca809b98
bit-yuyuyu <2677343384@qq.com>
2023-08-30 11:46:05 +0800

    创建Request框架

--- src/main/java/entity/Request.java
+++ src/main/java/entity/Request.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 public class Request {
     private String action;
-    private Map<String, Object> attributesMap;
+    /*
+    定义用户的属性，这里Long代表的为用户id，Object为用户实体属性
+     */
+    private Map<Long, Object> attributesMap;
 
     public Request() {
-        this.attributesMap = new HashMap<String, Object>();
+        this.attributesMap = new HashMap<Long, Object>();
     }
 
     public String getAction() {
@@ -18,19 +21,19 @@ public class Request {
         this.action = action;
     }
 
-    public Map<String, Object> getAttributesMap() {
+    public Map<Long, Object> getAttributesMap() {
         return attributesMap;
     }
 
-    public Object getAttribute(String name) {
+    public Object getAttribute(Long name) {
         return this.attributesMap.get(name);
     }
 
-    public void setAttribute(String name, Object value) {
+    public void setAttribute(Long name, Object value) {
         this.attributesMap.put(name, value);
     }
 
-    public void removeAttribute(String name) {
+    public void removeAttribute(Long name) {
         this.attributesMap.remove(name);
     }
 
