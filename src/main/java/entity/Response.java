c07c30f3e68184622080f53dbc1689e650f03c96
bit-yuyuyu <2677343384@qq.com>
2023-08-30 11:45:29 +0800

    创建Response框架

--- src/main/java/entity/Response.java
+++ src/main/java/entity/Response.java
@@ -6,23 +6,26 @@ import java.util.Map;
 
 public class Response {
     private ResponseStatus status;
-    private Map<String, Object> datamap;
+    /*
+    定义用户的属性，这里Long代表的为用户id，Object为用户实体属性
+     */
+    private Map<Long, Object> datamap;
 
     private OutputStream outputStream;
 
     public Response() {
         this.status = ResponseStatus.OK;
-        this.datamap = new HashMap<String, Object>();
+        this.datamap = new HashMap<Long, Object>();
     }
 
     public ResponseStatus getStatus() { return status; }
     public void setStatus(ResponseStatus status) { this.status = status; }
-    public Map<String, Object> getDataMap() { return datamap; }
-    public void setDataMap(Map<String, Object> datamap) { this.datamap = datamap; }
+    public Map<Long, Object> getDataMap() { return datamap; }
+    public void setDataMap(Map<Long, Object> datamap) { this.datamap = datamap; }
     public OutputStream getOutputStream() { return outputStream; }
     public void setOutputStream(OutputStream outputStream) { this.outputStream = outputStream; }
-    public void setData(String name, Object value) {this.datamap.put(name, value); }
-    public void getData(String name) { this.datamap.get(name); }
-    public void removeData(String name) {this.datamap.remove(name); }
+    public void setData(Long name, Object value) {this.datamap.put(name, value); }
+    public void getData(Long name) { this.datamap.get(name); }
+    public void removeData(Long name) {this.datamap.remove(name); }
     public void clearData() { this.datamap.clear(); }
 }
