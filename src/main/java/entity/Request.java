585ee36c0564d285cc0114f7d391fbc7ceed7ef5
bit-yuyuyu <2677343384@qq.com>
2023-08-30 10:55:52 +0800

    创建Request架构

---
+++ src/main/java/entity/Request.java
@@ -0,0 +1,32 @@
+package entity;
+
+import java.util.HashMap;
+import java.util.Map;
+public class Request {
+    private String action;
+    private Map<String, Object> attributesMap;
+    public Request() {
+        this.attributesMap = new HashMap<String, Object>();
+    }
+    public String getAction() {
+        return action;
+    }
+    public void setAction(String action) {
+        this.action = action;
+    }
+    public Map<String, Object> getAttributesMap() {
+        return attributesMap;
+    }
+    public Object getAttribute(String name) {
+        return this.attributesMap.get(name);
+    }
+    public void setAttribute(String name, Object value) {
+        this.attributesMap.put(name, value);
+    }
+    public void removeAttribute(String name) {
+        this.attributesMap.remove(name);
+    }
+    public void clearAttribute() {
+        this.attributesMap.clear();
+    }
+}
