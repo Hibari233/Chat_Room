ee643c642c4fa8c23b31b64600c651d549a224df
Hibari233 <592603453@qq.com>
2023-08-30 11:35:21 +0800

    创建User Controller控制器

--- src/main/java/server/MainServer.java
+++ src/main/java/server/MainServer.java
@@ -1,10 +1,11 @@
 package server;
 
-import com.sun.security.ntlm.Server;
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import server.controller.RequestProcessor;
+
+
 public class MainServer {
     public static void main(String[] args) {
         int port = 8024;
@@ -28,7 +29,7 @@ public class MainServer {
                                 + ":" + socket.getPort());
 
                         //针对每个客户端启动一个线程，在线程中调用请求处理器来处理每个客户端的请求
-                        // new Thread(new RequestProcessor(socket)).start();
+                         new Thread(new RequestProcessor(socket)).start();
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
---
+++ src/main/java/server/controller/UserController.java
@@ -0,0 +1,14 @@
+package server.controller;
+
+import entity.User;
+
+public class UserController {
+    public User login(long id, String password) {
+        return null;
+    }
+    public void addUser(User user) {}
+    public void deleteUser(User user) {}
+    public User findUser(long id) {
+        return null;
+    }
+}
