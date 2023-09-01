6bb5c5e71acf0d3e33859a6b1c7b9c030a5ca73b
bit-yuyuyu <2677343384@qq.com>
2023-09-01 15:16:08 +0800

    创建online和register用户列表&导入JDBC依赖

---
+++ src/main/java/server/entity/RegisterUserTableModel.java
@@ -0,0 +1,44 @@
+package server.entity;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.*;
+
+public class RegisterUserTableModel extends AbstractTableModel {
+    private String[] attribute ={"QQ号", "密码", "用户名", "性别"};
+    private List<String[]> rows = new ArrayList<String[]>();
+    @Override
+    public int getRowCount() {
+        return this.rows.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return this.attribute.length;
+    }
+
+    @Override
+    public String getValueAt(int rowIndex, int columnIndex) {
+        return this.rows.get(rowIndex)[columnIndex];
+    }
+
+    public String getColumnName(int columnIndex) {
+        return this.attribute[columnIndex];
+    }
+
+    public void AddRow(String[] value) {
+        int rowIndex = rows.size();
+        rows.add(value);
+        fireTableRowsInserted(rowIndex, rowIndex);
+    }
+    public void RemoveRow(long id) {
+        int rowIndex = Integer.parseInt(null);
+        for(int i = 0; i < rows.size(); i++) {
+            if (String.valueOf(id).equals(getValueAt(i, 0))) {
+                rowIndex = i;
+                break;
+            }
+        }
+        rows.remove(rowIndex);
+        fireTableRowsDeleted(rowIndex, rowIndex);
+    }
+}
