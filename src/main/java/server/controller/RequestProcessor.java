1d731ebaa5e7e0dd230fd4e0ab39ae4a8fc9a747
Niceboo <112788352+Niceboo@users.noreply.github.com>
2023-09-02 20:59:01 +0800

    Update RequestProcessor.java

--- src/main/java/server/controller/RequestProcessor.java
+++ src/main/java/server/controller/RequestProcessor.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executor;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import entity.*;
 import server.DataBuffer;
 import server.OnlineClientIOcache;
